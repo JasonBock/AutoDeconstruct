e9bdf7bfcfd988602fa31c52450a008a11e6dd95
Jason Bock <jason.r.bock@outlook.com>
2022-03-11 09:00:29 -0600

    Updated package version and added integration test

---
+++ src/AutoDeconstruct.IntegrationTests/InheritanceTests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+
+namespace AutoDeconstruct.IntegrationTests;
+
+public static class InheritanceTests
+{
+	[Test]
+	public static void RunDeconstruct()
+	{
+		var id = 3;
+		var data = "data";
+
+		var target = new DerivedInheritance
+		{
+			Id = id,
+			Data = data
+		};
+
+		var (newData, newId) = target;
+
+		Assert.Multiple(() =>
+		{
+			Assert.That(newData, Is.EqualTo(target.Data));
+			Assert.That(newId, Is.EqualTo(target.Id));
+		});
+	}
+}
+
+public class BaseInheritance
+{
+	public int Id { get; set; }
+}
+
+public class DerivedInheritance
+	: BaseInheritance
+{
+	public string? Data { get; set; }
+}
