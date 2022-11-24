71f0ffd660e058f3faa1461165638c848a6bb016
Jason Bock <jason.r.bock@outlook.com>
2022-11-24 13:14:04 -0600

    #8 integration test is in

---
+++ src/AutoDeconstruct.IntegrationTests/GenericTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+namespace AutoDeconstruct.IntegrationTests;
+
+public static class GenericTests
+{
+	public class GenericThings<T1, T2>
+	{
+		public required T1 Thing1 { get; set; }
+		public required T2 Thing2 { get; set; }
+	}
+
+	[Test]
+	public static void DeconstructWithGenerics()
+	{
+		var thing = new GenericThings<int, string> { Thing1 = 1, Thing2 = "2" };
+		(var newThing1, var newThing2) = thing;
+
+		Assert.Multiple(() =>
+		{
+			Assert.That(newThing1, Is.EqualTo(1));
+			Assert.That(newThing2, Is.EqualTo("2"));
+		});
+	}
+}
