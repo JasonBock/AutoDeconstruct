f96e060d447fced0db15a79b2f6b8a6e59eff7c1
Jason Bock <jason.r.bock@outlook.com>
2022-11-24 13:08:12 -0600

    #8 tests are in for multiple generics and multiple constraints

--- changelog.md
+++ changelog.md
@@ -11,3 +11,6 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - Generated code now uses fully-qualified named (issue [#5](https://github.com/JasonBock/AutoDeconstruct/issues/5))
 - Records are now considered for `Deconstruct()` generation (issue [#4](https://github.com/JasonBock/AutoDeconstruct/issues/4))
 - Putting `[NoAutoDeconstruct]` on a type will signal AutoDeconstruct not to generate a `Deconstruct()` method (issue [#6](https://github.com/JasonBock/AutoDeconstruct/issues/6))
+
+### Fixed
+- Generic types are now handled correctly (issue [#8](https://github.com/JasonBock/AutoDeconstruct/issues/8))
--- src/AutoDeconstruct.Tests/GenericGeneratorTests.cs
+++ src/AutoDeconstruct.Tests/GenericGeneratorTests.cs
@@ -86,4 +86,91 @@ public static class GenericGeneratorTests
 			new[] { (typeof(AutoDeconstructGenerator), "Test_AutoDeconstruct.g.cs", generatedCode) },
 			Enumerable.Empty<DiagnosticResult>()).ConfigureAwait(false);
 	}
+
+	// TODO: Multiple generics
+	// and multiple constraints on multiple generics.
+	[Test]
+	public static async Task GenerateWhenTypeHasMultipleGenericsAsync()
+	{
+		var code =
+			"""
+			using System;
+
+			namespace TestSpace
+			{
+				public class Test<T1, T2>
+				{ 
+					public T1 Id { get; set; }
+					public T2 Value { get; set; }
+				}
+			}
+			""";
+
+		var generatedCode =
+			"""
+			#nullable enable
+			
+			namespace TestSpace;
+			
+			public static partial class TestExtensions
+			{
+				public static void Deconstruct<T1, T2>(this global::TestSpace.Test<T1, T2> self, out T1 id, out T2 value)
+				{
+					global::System.ArgumentNullException.ThrowIfNull(self);
+					(id, value) =
+						(self.Id, self.Value);
+				}
+			}
+			
+			""";
+
+		await TestAssistants.RunAsync(code,
+			new[] { (typeof(AutoDeconstructGenerator), "Test_AutoDeconstruct.g.cs", generatedCode) },
+			Enumerable.Empty<DiagnosticResult>()).ConfigureAwait(false);
+	}
+
+	[Test]
+	public static async Task GenerateWhenTypeHasMultipleGenericsWithMultipleConstraintsAsync()
+	{
+		var code =
+			"""
+			using System;
+
+			namespace TestSpace
+			{
+				public interface IAmForStruct { }
+
+				public class Test<T1, T2>
+					where T1 : struct, IAmForStruct
+					where T2 : unmanaged, IAmForStruct
+				{ 
+					public T1 Id { get; set; }
+					public T2 Value { get; set; }
+				}
+			}
+			""";
+
+		var generatedCode =
+			"""
+			#nullable enable
+			
+			namespace TestSpace;
+			
+			public static partial class TestExtensions
+			{
+				public static void Deconstruct<T1, T2>(this global::TestSpace.Test<T1, T2> self, out T1 id, out T2 value)
+					where T1 : struct, global::TestSpace.IAmForStruct where T2 : unmanaged, global::TestSpace.IAmForStruct
+				{
+					global::System.ArgumentNullException.ThrowIfNull(self);
+					(id, value) =
+						(self.Id, self.Value);
+				}
+			}
+			
+			""";
+
+		await TestAssistants.RunAsync(code,
+			new[] { (typeof(AutoDeconstructGenerator), "Test_AutoDeconstruct.g.cs", generatedCode) },
+			Enumerable.Empty<DiagnosticResult>()).ConfigureAwait(false);
+	}
 }
