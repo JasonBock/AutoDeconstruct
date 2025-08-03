0915c27e8618ce625aa22e9b186b954f24e14a23
Jason Bock <jason.r.bock@outlook.com>
2025-08-03 10:14:56 -0500

    #22 started work on the refactoring
    Related Work Items: #3

---
+++ src/AutoDeconstruct.Completions/AddAttributeRefactoring.cs
@@ -0,0 +1,88 @@
+﻿using AutoDeconstruct.Completions.Extensions;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeRefactorings;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Composition;
+using System.Xml.Linq;
+
+namespace AutoDeconstruct.Completions;
+
+/// <summary>
+/// Suggests refactorings to create the
+/// <c>AutoDeconstructAttribute</c> or <c>TargetAutoDeconstructAttribute</c> declaration for the type
+/// at the current code location (if possible).
+/// </summary>
+[ExportCodeRefactoringProvider(LanguageNames.CSharp,
+	Name = nameof(AddAttributeRefactoring))]
+[Shared]
+public sealed class AddAttributeRefactoring
+	: CodeRefactoringProvider
+{
+	/// <inheritdoc />
+	public override async Task ComputeRefactoringsAsync(CodeRefactoringContext context)
+	{
+		var document = context.Document;
+
+		if (document.Project.Solution.Workspace.Kind == WorkspaceKind.MiscellaneousFiles) { return; }
+
+		var solution = document.Project.Solution;
+		var span = context.Span;
+		var cancellationToken = context.CancellationToken;
+		var model = await document.GetSemanticModelAsync(cancellationToken);
+
+		if (model is null) { return; }
+
+		var root = (CompilationUnitSyntax)await model.SyntaxTree.GetRootAsync(cancellationToken);
+		var node = root.FindNode(span);
+		var deconstructTypeSymbol = node.FindParentSymbol(model, cancellationToken);
+
+		if (deconstructTypeSymbol is not null)
+		{
+			var actions = new List<CodeAction>(2)
+			{
+			   // Create a [TargetAutoDeconstruct] code action regardless of whether the type exists
+			   // in the current compilation.
+			   AddAttributeRefactoring.CreateTargetAutoDeconstructAction()
+			};
+
+			// Only offer [AutoDeconstruct] if the type is in the
+			// current compilation
+			if (deconstructTypeSymbol.DeclaringSyntaxReferences.Length > 0)
+			{
+				var reference = deconstructTypeSymbol.DeclaringSyntaxReferences.Single(_ => _ == node.GetReference());
+
+				var typeDeclarationNode = (await reference.GetSyntaxAsync()) as TypeDeclarationSyntax;
+
+				if (typeDeclarationNode is not null)
+				{
+					var typeDeclarationDocument = solution.GetDocument(typeDeclarationNode.SyntaxTree);
+
+					if (typeDeclarationDocument is not null)
+					{
+						actions.Add(AddAttributeRefactoring.CreateAutoDeconstructAction(
+							root, typeDeclarationNode, typeDeclarationDocument));
+					}
+				}
+			}
+
+			context.RegisterRefactoring(
+				CodeAction.Create("Add auto-deconstruction ...", [.. actions], false));
+		}
+	}
+
+	private static CodeAction CreateTargetAutoDeconstructAction()
+	{
+
+	}
+
+	private static CodeAction CreateAutoDeconstructAction(CompilationUnitSyntax currentRoot,
+		TypeDeclarationSyntax typeDeclarationNode, Document document)
+	{
+		var newTypeDeclarationNode = typeDeclarationNode.AddAttributeLists([]);
+		var newRoot = currentRoot.ReplaceNode(typeDeclarationNode, newTypeDeclarationNode);
+
+		return CodeAction.Create("[AutoDeconstruct]",
+			token => Task.FromResult(document.WithSyntaxRoot(newRoot)));
+	}
+}
---
+++ src/AutoDeconstruct.Completions/Extensions/SyntaxNodeExtensions.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace AutoDeconstruct.Completions.Extensions;
+
+internal static class SyntaxNodeExtensions
+{
+	internal static INamedTypeSymbol? FindParentSymbol(this SyntaxNode self,
+		SemanticModel model, CancellationToken cancellationToken)
+	{
+		Type[] syntaxNodeTypes =
+			[
+				typeof(IdentifierNameSyntax),
+				typeof(ParameterSyntax),
+				typeof(ObjectCreationExpressionSyntax),
+				typeof(BaseTypeSyntax),
+				typeof(PredefinedTypeSyntax),
+				typeof(ClassDeclarationSyntax),
+				typeof(InterfaceDeclarationSyntax),
+				typeof(RecordDeclarationSyntax)
+			];
+
+		var parent = self;
+
+		while (parent is not null)
+		{
+			var syntaxNodeTypeMatch = syntaxNodeTypes.FirstOrDefault(
+				t => t.IsAssignableFrom(parent.GetType()));
+
+			if (syntaxNodeTypeMatch is not null)
+			{
+				ISymbol? parentSymbol = null;
+
+				if (parent is IdentifierNameSyntax identifierNameSyntax)
+				{
+					var identifierNameSymbol = model.GetSymbolInfo(identifierNameSyntax, cancellationToken);
+
+					if (identifierNameSymbol.Symbol is not null)
+					{
+						parentSymbol = identifierNameSymbol.Symbol;
+					}
+					else if (identifierNameSymbol.CandidateSymbols.Length > 0)
+					{
+						parentSymbol = identifierNameSymbol.CandidateSymbols[0];
+					}
+				}
+				else if (parent is ParameterSyntax parameterSyntax)
+				{
+					var parameterSymbol = model.GetDeclaredSymbol(parameterSyntax, cancellationToken) as IParameterSymbol;
+
+					if (parameterSymbol is not null)
+					{
+						parentSymbol = parameterSymbol.Type;
+					}
+				}
+				else if (parent is ObjectCreationExpressionSyntax objectCreationExpressionSyntax)
+				{
+					var objectCreationSymbol = model.GetSymbolInfo(objectCreationExpressionSyntax, cancellationToken).Symbol as IMethodSymbol;
+
+					if (objectCreationSymbol is not null)
+					{
+						parentSymbol = objectCreationSymbol.ContainingType;
+					}
+				}
+				else if (parent is BaseTypeSyntax baseTypeSyntax)
+				{
+					parentSymbol = model.GetSymbolInfo(baseTypeSyntax.Type, cancellationToken).Symbol;
+				}
+				else if (parent is PredefinedTypeSyntax predefinedTypeSyntax)
+				{
+					parentSymbol = model.GetSymbolInfo(predefinedTypeSyntax, cancellationToken).Symbol;
+				}
+				else
+				{
+					parentSymbol = parent switch
+					{
+						ClassDeclarationSyntax classDeclarationSyntax =>
+							model.GetDeclaredSymbol(classDeclarationSyntax, cancellationToken),
+						InterfaceDeclarationSyntax interfaceDeclarationSyntax =>
+							model.GetDeclaredSymbol(interfaceDeclarationSyntax, cancellationToken),
+						RecordDeclarationSyntax recordDeclarationSyntax =>
+							model.GetDeclaredSymbol(recordDeclarationSyntax, cancellationToken),
+						_ => null
+					};
+				}
+
+				if (parentSymbol is INamedTypeSymbol namedTypeSymbol)
+				{
+					return namedTypeSymbol;
+				}
+			}
+
+			parent = parent.Parent;
+		}
+
+		return null;
+	}
+}
