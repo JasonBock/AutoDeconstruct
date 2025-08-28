8b7d9ba3c3672d09236a98fe6476103dda1f37ae
Jason Bock <jason.r.bock@outlook.com>
2025-08-28 10:55:21 -0500

    #24 I was overthinking validation in the attribute

--- src/AutoDeconstruct/AutoDeconstructAttribute.cs
+++ src/AutoDeconstruct/AutoDeconstructAttribute.cs
@@ -11,5 +11,47 @@ public sealed class AutoDeconstructAttribute
 	/// <summary>
 	/// Creates a new <see cref="AutoDeconstructAttribute"/> instance.
 	/// </summary>
-	public AutoDeconstructAttribute() { }
+	public AutoDeconstructAttribute()
+		: this(Filtering.None, []) { }
+
+	/// <summary>
+	/// Creates a new <see cref="AutoDeconstructAttribute"/> instance,
+	/// specifying filtered properties.
+	/// </summary>
+	/// <param name="filtering">The filter setting.</param>
+	/// <param name="properties">The filtered properties, specified by name.</param>
+	/// <exception cref="ArgumentNullException">Thrown if <paramref name="properties"/> is <see langword="null" />.</exception>
+	/// <exception cref="ArgumentException">
+	/// Thrown if <paramref name="filtering"/> is <see cref="Filtering.None"/> and values exist in <paramref name="properties"/>,
+	/// or filtering is enabled and no values exist in <paramref name="properties"/>.
+	/// </exception>
+	public AutoDeconstructAttribute(Filtering filtering, string[] properties)
+	{
+		this.Validate(filtering, properties);
+		(this.Filtering, this.Properties) = (filtering, properties);
+	}
+
+	private void Validate(Filtering filtering, string[] properties)
+	{
+		if (properties is null) { throw new ArgumentNullException(nameof(properties)); }
+
+		if (filtering == Filtering.None && properties.Length > 0)
+		{
+			throw new ArgumentException("Properties should not be provided if no filtering occurs.", nameof(properties));
+		}
+		else if (properties.Length == 0)
+		{
+			throw new ArgumentException("Properties must be provided if filtering occurs.", nameof(properties));
+		}
+	}
+
+	/// <summary>
+	/// Gets the filter setting.
+	/// </summary>
+	public Filtering Filtering { get; private set; }
+
+	/// <summary>
+	/// Gets the filtered properties, if any were specified.
+	/// </summary>
+	public string[] Properties { get; private set; }
 }
---
+++ src/AutoDeconstruct/Filtering.cs
@@ -0,0 +1,20 @@
+﻿namespace AutoDeconstruct;
+
+/// <summary>
+/// Used to denote if named properties should be included or excluded.
+/// </summary>
+public enum Filtering
+{
+	/// <summary>
+	/// No filtering is done.
+	/// </summary>
+	None,
+	/// <summary>
+	/// Only the specified properties are included.
+	/// </summary>
+	Include,
+	/// <summary>
+	/// The specified properties are excluded.
+	/// </summary>
+	Exclude
+}
