64afd8e1be6cb5c6420022fe8eaa23b7a9b34270
Jason Bock <jason.r.bock@outlook.com>
2025-09-18 16:43:44 -0500

    Added a scenarios project

---
+++ src/AutoDeconstruct.Scenarios/Person.cs
@@ -0,0 +1,12 @@
+﻿namespace AutoDeconstruct.Scenarios;
+
+public sealed class Person
+{
+	public Person(string name, uint age, Guid id) =>
+		(this.Name, this.Age, this.Id) = 
+			(name, age, id);
+
+	public uint Age { get; }
+	public Guid Id { get; }
+	public string Name { get; }
+}
